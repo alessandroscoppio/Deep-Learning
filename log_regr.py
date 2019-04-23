0cb31f4d98d2004cc077d7d9e152fe170af18011
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2019-04-23 16:58:55 +0200

    Preparation of dataset outside, sepearted training_set and output_set

--- log_regr.py
+++ log_regr.py
@@ -17,38 +17,46 @@ def sigmoid_derivate(z):
     return sig * (1 - sig)
 
 
-def replace_class_on_dataset(dataset):
+def replace_class_on_dataset(dataset, class_index):
     class_mapping = {}
     unique_count = 0
     for row in dataset:
-        if not row[4] in class_mapping:
-            class_mapping[row[4]] = unique_count
-            row[4] = unique_count
+        if not row[class_index] in class_mapping:
+            class_mapping[row[class_index]] = unique_count
+            row[class_index] = unique_count
             unique_count += 1
         else:
-            row[4] = class_mapping[row[4]]
+            row[class_index] = class_mapping[row[class_index]]
 
     return dataset
 
 
+def prepare_dataset(dataset, training_data_size, features_combination, class_index):
+    # Truncate dataset
+    dataset = dataset[:training_data_size]
+
+    # Replace classes on the last column with number
+    dataset = replace_class_on_dataset(dataset, class_index)
+
+    # Create training set by selecting rows and columns and copying
+    training_set = dataset[:, features_combination].copy()
+
+    # Create output class vector
+    output_set = dataset[:, class_index].copy()
+
+    return training_set, output_set
+
+
+def normalize_dataset(training_set):
+    # Normalize using min-max scaling
+    return pre.minmax_scale(training_set)
+
+
 class LogisticRegression:
 
-    def __init__(self, dataset, training_data_size, features_combination, learning_rate=0.9, regularization_term=0.7):
-        # Number of dataset features
-        self.nr_features = len(features_combination)
-
-        # Truncate dataset
-        self.dataset = dataset[:training_data_size]
-
-        # Replace classes on the last column with number
-        self.dataset = replace_class_on_dataset(self.dataset)
-
-        # Create training set by selecting rows and columns and copying
-        self.training_set = self.dataset[:, features_combination].copy()
-
-        # Normalize using min-max scaling
-        self.training_set = pre.minmax_scale(self.training_set)
-
+    def __init__(self, training_set, output_set, features_combination, learning_rate=0.9, regularization_term=0.7):
+        self.training_set = training_set
+        self.output_set = output_set
         self.learning_rate = learning_rate
         self.regularization_term = regularization_term
 
@@ -61,8 +69,11 @@ class LogisticRegression:
 
         self.input_weights = np.random.uniform(0, 0.2, input_size[1]+1)
 
-    def forward(self):
+    def train_network(self):
+        prediction = self.forward()
+        # Compare with output_set 
 
+    def forward(self):
         z = np.dot(self.input_layer, self.input_weights)
         prediction = sigmoid(z)
 
@@ -87,15 +98,25 @@ if __name__ == "__main__":
     # Retrieve Dataset
     dataset = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
                           header=None).values
-    # Specify parameters
-    features_combination = [0, 2, 4]  # 4 is class
+    # Specify dataset parameters
+    features_combination = [0, 2]
+    class_index = 4
+
+    # Prepare dataset
+    training_set, output_set = prepare_dataset(dataset,
+                                               training_data_size=100,
+                                               features_combination=features_combination,
+                                               class_index=class_index)
+    # Normalize dataset
+    training_set = normalize_dataset(training_set)
+
+    # Specify training parameters
     learning_rate = 0.9
     regularization_term = 0
 
     # Initiate Model
-    logistic_regression = LogisticRegression(dataset,
-                                             training_data_size=100,
-                                             features_combination=features_combination,
+    logistic_regression = LogisticRegression(training_set=training_set,
+                                             output_set=output_set,
                                              learning_rate=learning_rate,
                                              regularization_term=regularization_term)
-    logistic_regression.forward()
+    logistic_regression.train_network()
