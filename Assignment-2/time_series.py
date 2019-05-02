042d0f688d58fa001a0de43cf6105e5f0ff1dac9
Alessandro Scoppio <alessandroscoppio@gmail.com>
2019-05-02 19:29:27 +0200

    finished multilayerperceptron model

--- Assignment-2/time_series.py
+++ Assignment-2/time_series.py
@@ -20,25 +20,35 @@ def prepare_data(data, window_size):
     for idx in range(len(data) - window_size - 1):
         batches.append(data[idx: idx + window_size])
         labels.append(data[idx + window_size])
-    return np.asarray(batches), np.asarray(labels)
+    return np.array(batches), np.array(labels)
 
 
 # define dataset
 series = np.array(scipy.io.loadmat('Xtrain.mat')['Xtrain'])
-# plot_series(1000, series, 'Original Data')
+# plot training set
+plot_series(1000, series, 'Original Data')
 
-batches, labels = prepare_data(series, 50)
-print(batches)
-print(labels)
-# # define model
-# model = Sequential()
-# model.add(Dense(100, activation='relu', input_dim=100))
-# model.add(Dense(1))
-# model.compile(optimizer='adam', loss='mse')
-# # fit model
-# model.fit(X, y, epochs=2000, verbose=0)
-# # demonstrate prediction
-# x_input = np.array([50, 60, 70])
-# x_input = x_input.reshape((1, 3))
-# yhat = model.predict(x_input, verbose=0)
-# print(yhat)
+# define window size
+window_size = 50
+
+# apply window size to construct a batches of training data and expected prediction in labels
+batches, labels = prepare_data(series, window_size)
+
+# use as input all batches but the last one, to use as test
+batches = batches[:, :, 0]
+X = batches[:-1]
+y = labels[:-1]
+
+
+# define model
+model = Sequential()
+model.add(Dense(100, activation='relu', input_dim=(window_size)))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+# fit model
+model.fit(X, y, epochs=1000, verbose=1)
+# demonstrate prediction
+x_input = batches[-1]
+x_input = x_input.reshape((1, window_size))
+prediction = model.predict(x_input, verbose=1)
+print("Predicted: ", prediction, "    Expected: ", labels[-1])
