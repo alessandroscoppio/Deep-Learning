a2f6b22e39c6abc50e0cbf6fdd87bf29d9aabe1d
Alessandro Scoppio <alessandroscoppio@gmail.com>
2019-05-02 21:54:18 +0200

    little changes in cnn for allowing simulation mode

--- Assignment-2/models.py
+++ Assignment-2/models.py
@@ -29,17 +29,17 @@ class CNNModel:
     def __init__(self, input_size):
         self.input_size = input_size
         self.model = Sequential()
-        self.model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(self.input_size, 1)))
+        self.model.add(Conv1D(filters=64, kernel_size=2, activation='relu'))
         self.model.add(MaxPooling1D(pool_size=2))
         self.model.add(Flatten())
         self.model.add(Dense(50, activation='relu'))
         self.model.add(Dense(1))
         self.model.compile(optimizer='adam', loss='mse')
 
-    def fit(self, X, y):
-        self.model.fit(X, y, epochs=1000, verbose=1)
+    def fit(self, X, y, epochs):
+        self.model.fit(X, y, epochs=epochs, verbose=1)
 
     def predict(self, x):
         x_input = x.reshape((1, self.input_size, 1))
         prediction = self.model.predict(x_input, verbose=0)
-        return prediction
+        return prediction[0]
--- Assignment-2/time_series.py
+++ Assignment-2/time_series.py
@@ -6,6 +6,7 @@ import scipy.io
 import matplotlib.pyplot as plt
 from models import *
 
+
 def plot_series(timesteps, values, title):
     x = range(timesteps)
     y = values
@@ -27,9 +28,9 @@ def simulation_mode(original_input, model, steps):
     input = original_input
     predictions = []
     for step in range(steps):
-        prediction = model.predict(input)
+        prediction = model.predict(input).round()
         predictions.append(prediction)
-        new_input = np.zeros(len(input))
+        new_input = np.zeros((len(input), 1))
         new_input[:-1] = input[1:]
         new_input[-1] = prediction
         input = new_input
@@ -40,13 +41,13 @@ def simulation_mode(original_input, model, steps):
 # define dataset
 series = np.array(scipy.io.loadmat('Xtrain.mat')['Xtrain'])
 # plot training set
-plot_series(1000, series, 'Original Data')
+# plot_series(1000, series, 'Original Data')
 
 # define window size
 window_size = 50
 
 # define epochs
-epochs = 1000
+epochs = 2000
 
 # apply window size to construct a batches of training data and expected prediction in labels
 batches, labels = prepare_data(series, window_size)
@@ -56,20 +57,22 @@ CNN Model
 """
 
 # Model
-model = CNNModel()
+# model = CNNModel(window_size)
 # Fit model with all data except last one
-model.fit(batches[:-1], labels[:-1])
+# model.fit(batches[:-1], labels[:-1], epochs)
 # Use last one to predict
-model.predict(batches[-1], labels[-1])
+# prediction = model.predict(batches[-1])
+# print("Predicted: {0}\nExpecred:  {1}".format(prediction, labels[-1]))
 
-# use as input all batches but the last one, to use as test
-batches = batches[:, :, 0]
-X = batches[:-1]
-y = labels[:-1]
 
-# define model
-MLP_model = MLPModel(window_size)
+# # use as input all batches but the last one, to use as test
+# batches = batches[:, :, 0]
+# X = batches[:-1]
+# y = labels[:-1]
 
+# # Multi-Layer Perceptron Model
+# MLP_model = MLPModel(window_size)
+#
 # # train
 # MLP_model.fit(X, y, epochs)
 #
@@ -82,6 +85,7 @@ MLP_model = MLPModel(window_size)
 steps = 200
 train_set = batches[:-200]
 train_labels = labels[:-200]
-MLP_model.fit(train_set, train_labels, epochs)
-predictions = simulation_mode(batches[-200], MLP_model, steps)
+sim_model = CNNModel(window_size)
+sim_model.fit(train_set, train_labels, epochs)
+predictions = simulation_mode(batches[-200], sim_model, steps)
 print(predictions)
